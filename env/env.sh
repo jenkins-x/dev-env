a01b88d1a78278267db7c2ebb70b7148332826d5
cagiti <caicooper82@gmail.com>
2019-02-05 14:17:07 +0000

    Refactored goland script to align to new environment setup

--- env/env.sh
+++ env/env.sh
@@ -1,3 +1,5 @@
+GOLAND="rycus86/goland:latest"
+
 if [[ "${1}" == "mac" ]]
 then
   # mac environment variables
@@ -6,6 +8,7 @@ then
   WORKSPACE=~/Development/workspace
   SSH=~/.ssh
   SUDO=""
+  X11_ERROR="DISPLAY environment not set, is X11 installed? See https://www.xquartz.org/releases/index.html for more information"
 else
   # all other environments
   DOCKER_SOCKET=/var/run/docker.sock
@@ -13,4 +16,5 @@ else
   WORKSPACE=~/workspace
   SSH=~/.ssh
   SUDO="sudo"
+  X11_ERROR="Not supported"
 fi
