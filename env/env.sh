9a21a7061526f07ecdf57beb5b0a7b57c3788fba
Cai Cooper <caicooper82@gmail.com>
2019-02-12 23:38:16 +0000

    Restructured and Refactored goland script

--- env/env.sh
+++ env/env.sh
@@ -9,6 +9,8 @@ then
   SSH=~/.ssh
   SUDO=""
   X11_ERROR="DISPLAY environment not set, is X11 installed? See https://www.xquartz.org/releases/index.html for more information"
+  XHOST="/opt/X11/bin/xhost"
+  XQUARTZ_ERROR="Unable to locate xhost, please visit https://www.xquartz.org/ to install XQuartz"
 else
   # all other environments
   DOCKER_SOCKET=/var/run/docker.sock
@@ -17,4 +19,5 @@ else
   SSH=~/.ssh
   SUDO="sudo"
   X11_ERROR="Not supported"
+  XQUARTZ_ERROR="Not supported"
 fi
